df7ca570f29e0d484c99dea4585127ca7fee31c5
Pascal Brunot <pascal.brunot@gmail.com>
2025-01-12 20:47:52 +0100

    Update laser-opencv.py

--- tests/laser-opencv.py
+++ tests/laser-opencv.py
@@ -3,25 +3,31 @@
 import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
-cap.set(cv2.CAP_PROP_EXPOSURE, -7)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1680)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-cap.set(cv2.CAP_PROP_FPS, 10.0)
+cap.set(cv2.CAP_PROP_FPS, 5.0)
+cap.set(cv2.CAP_PROP_EXPOSURE, -7)
 
 pts = []
 cpt = 0
-last_pos = None
+last_pos = (512,369)
 
 while (1):
     cpt += 1
     # Take each frame
     ret, frame = cap.read()
     
-    _ , _ ,channel=cv2.split(frame)
+    (B, G, R) = cv2.split(frame)
+    zeros = np.zeros(frame.shape[:2], dtype="uint8")
+    cv2.imshow("Red", cv2.merge([zeros, zeros, R]))
+    cv2.imshow("Green", cv2.merge([zeros, G, zeros]))
+    cv2.imshow("Blue", cv2.merge([B, zeros, zeros]))
+    channel = G.copy()
+
     #cv2.GaussianBlur(g, (5,5), 0, g)
 
-    r,diff_thr = cv2.threshold(channel, 200, 255, cv2.THRESH_BINARY)
+    r,diff_thr = cv2.threshold(channel, 200, 255, cv2.THRESH_TOZERO)
     
     masked_channel = cv2.bitwise_and(channel, channel, None, diff_thr)
     
@@ -30,25 +36,25 @@ while (1):
     output = frame.copy()
 
     circles = cv2.HoughCircles(masked_channel, cv2.HOUGH_GRADIENT, 1, minDist=50,
-                             param1=50,param2=2,minRadius=2,maxRadius=10)
+                             param1=50,param2=2,minRadius=3,maxRadius=10)
 
     if circles is not None and len(circles) > 0 and len(circles[0]) > 0:
         circle = circles[0]
 
-        if last_pos is None:
-            last_pos = (int(circle[0]),int(circle[0]))
-        
-        if len(circles) > 1:
-            #find the position closest to the last position
-            min_dist = 100000
-            best_pos = last_pos
-            for circle in circles[0,:]:
-                dist = np.linalg.norm(np.array([circle[0],circle[1]]) - np.array(last_pos))
-                if dist < min_dist:
-                    min_dist = dist
-                    best_pos = np.array([circle[0],circle[1]])
-            last_pos = best_pos
+        #find the position closest to the last position
+        min_dist = 100000
+        best_pos = last_pos
+        for circle in circles[0,:]:
+            dist = np.linalg.norm(np.array([circle[0],circle[1]]) - np.array(last_pos))
+            if dist < min_dist:
+                min_dist = dist
+                best_pos = np.array([circle[0]+circle[2]/2,circle[1]+circle[2]/2])
+        last_pos = (int(best_pos[0]), int(best_pos[1]))
+        found = True
+    else:
+        found = False
 
+    if found:
         cv2.circle(img=output, center=last_pos, radius=1, color=(0,255,0), thickness=2)
         print(f"Laser position={last_pos}")
         
